d73b161f269dbe45e49555dad7026f8e6c147af5
Giulio De Matteis <hello@giuliodematte.is>
2023-05-19 21:53:33 +0200

    Changed error output

--- memos/echo.py
+++ memos/echo.py
@@ -8,7 +8,7 @@ def echo_command(memo_id):
     """Prints the content of a memo"""
     memo = Memo().get(memo_id)
     if memo is None:
-        click.echo(f'Cannot retrieve memo with id {memo_id}', err=True)
+        click.echo(f'Cannot fetch memo with id {memo_id}', err=True)
         return
     
     click.echo(memo['content'])
