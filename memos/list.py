0993a1f326368f0744b2c4558b96aa171d333999
Giulio De Matteis <hello@giuliodematte.is>
2023-05-19 19:43:46 +0200

    Added list cmd

--- memos/list.py
+++ memos/list.py
@@ -1,7 +1,26 @@
 import click
+from prettytable import PrettyTable
+from client.memo import Memo
 
 
 @click.command(name='list')
-def list_command():
+@click.option('-p', '--public', is_flag=True, default=False, help='Show public memos')
+def list_command(public):
     """Lists memos"""
-    pass
+    model = Memo()
+    if public:
+        memos = model.get('all')
+    else:
+        memos = model.get('')
+    if memos is None:
+        click.echo('Cannot fetch memos', err=True)
+    else:
+        t = PrettyTable()
+        t.align = 'l'
+        t.field_names = ['#', 'CREATOR', 'CONTENT']
+        for memo in memos:
+            content = memo['content'].split()[:5]
+            content = ' '.join(content) + '...'
+            t.add_row([memo['id'], memo['creatorName'], content])
+
+        print(t)
