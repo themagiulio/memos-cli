c14a7b6854f12912aa56b34233829a5f35328567
Giulio De Matteis <hello@giuliodematte.is>
2023-05-21 21:06:08 +0200

    Added version cmd

---
+++ memos/__init__.py
@@ -0,0 +1,3 @@
+import pkg_resources
+
+__VERSION__ = pkg_resources.get_distribution('memos-cli').version
--- memos/cli.py
+++ memos/cli.py
@@ -1,5 +1,6 @@
 import click
 from click_aliases import ClickAliasedGroup
+from memos import __VERSION__
 from memos.auth import auth_command
 from memos.echo import echo_command
 from memos.edit import edit_command
@@ -7,10 +8,16 @@ from memos.list import list_command
 from memos.new import new_command
 from memos.remove import remove_command
 
-
 @click.group(cls=ClickAliasedGroup)
 def cli():
     pass
+
+
+@click.command(name='version')
+def version_command():
+    """Prints Memos CLI version"""
+    click.echo(f'Memos CLI v{__VERSION__}')
+
 
 cli.add_command(auth_command)
 cli.add_command(echo_command)
@@ -18,6 +25,7 @@ cli.add_command(edit_command)
 cli.add_command(list_command)
 cli.add_command(new_command)
 cli.add_command(remove_command)
+cli.add_command(version_command)
 
 if __name__ == '__main__':
     cli()
