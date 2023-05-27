035d09c246a6b81ecec6284120949a960fb9c8d3
Giulio De Matteis <hello@giuliodematte.is>
2023-05-27 15:03:41 +0200

    Added confirm prompt for remove cmd

--- memos/remove.py
+++ memos/remove.py
@@ -6,6 +6,9 @@ from client.memo import Memo
 @click.argument('memo_id')
 def remove_command(memo_id):
     """Removes a memo"""
+    confirm = click.confirm('Are you sure you want to remove this memo?')
+    if not confirm:
+        return
     deleted = Memo().delete(memo_id)
     if not deleted:
         click.echo(f'Cannot remove memo with id {memo_id}', err=True)
